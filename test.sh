c6a771450195023c529b2b9e4941d75079057f57
Eric Huss <eric@huss.org>
2024-01-10 10:24:11 -0800

    Macos test

--- test.sh
+++ test.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-set -ex
+set -euo pipefail
+IFS=$'\n\t'
 
-rustup install 1.71.1
-rustup default 1.71.1
-cargo -Vv
-
-export CARGO_NET_RETRY=0
-export CARGO_HTTP_MULTIPLEXING=false
-
-for package in cargo cargo-make cargo-all-features cargo-deb backtrace deno leptos_icons poem
-do
-    echo $package
-    cargo new a
-    cd a
-    echo "$package = \"*\"" >> Cargo.toml
-    CARGO_LOG=network=trace,cargo::ops::registry=trace,cargo::core::package=trace,cargo::util::network=trace,cargo::sources::registry=trace \
-        CARGO_HTTP_DEBUG=true \
-        CARGO_HOME=chome \
-        cargo generate-lockfile
-    cd ..
-    rm -rf a
-done
+csrutil status
+# Gatekeeper
+spctl --status
