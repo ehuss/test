db7a0b9eeb76ace521c5bccf538184256adb9c7f
Eric Huss <eric@huss.org>
2025-05-07 13:26:01 -0700

    Swap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     let retry_after = "Fri, 01 Jan 2100 00:00:00 GMT";
     // Second option: Retry-After is a future HTTP date string that tells us when to retry.
+
+    let retry_time = jiff::fmt::rfc2822::parse(retry_after).unwrap();
+    let actual = jiff::Timestamp::now()
+        .until(&retry_time)
+        .unwrap()
+        .total(jiff::Unit::Millisecond)
+        .unwrap() as u64;
     let expected = jiff::Zoned::now()
         .until(
             &jiff::civil::date(2100, 1, 1)
@@ -8,13 +15,6 @@ fn main() {
                 .to_zoned(jiff::tz::TimeZone::UTC)
                 .unwrap(),
         )
-        .unwrap()
-        .total(jiff::Unit::Millisecond)
-        .unwrap() as u64;
-
-    let retry_time = jiff::fmt::rfc2822::parse(retry_after).unwrap();
-    let actual = jiff::Timestamp::now()
-        .until(&retry_time)
         .unwrap()
         .total(jiff::Unit::Millisecond)
         .unwrap() as u64;
